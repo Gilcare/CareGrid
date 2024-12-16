f87c063a75ad72be5f5484a51ca7080561d765b9
Gilcare <gilcare.com@gmail.com>
2024-12-16 14:42:43 +0100

    Update CareGrid.py

--- CareGrid/CareGrid.py
+++ CareGrid/CareGrid.py
@@ -14,11 +14,18 @@ def main():
     reg_px, view_ehr  = st.columns(2, vertical_alignment = "bottom" )
     reg_px.button("Register New Patient")
     view_ehr.button("Access Patient's Records")
-    #if reg_px.butt is 
+    if reg_px.button():
+      #...INSERT CODE TO ASSIGN ID TO NEW PATIENT...
+      with st.form(key = "Register Patient Details",clear_on_submit = True):
+        st.subheader("Register Patient Details")
+        patient_name = st.text_input("Name")
+        patient_age = st.number_input("Age")
+        patient_sex = st.checkbox("Male")
+      
 
   
   if app == "🗓️Schedule Appointment":
-    st.caption("🗓️_Schedule An Appointment With Your Doctor_")
+    st.caption(":date: _Schedule An Appointment With Your Doctor_")
     appointment = st.date_input("Enter Date")
 
 if __name__ == "__main__":
