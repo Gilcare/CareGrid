974e0e00a06869cf66f33e3020b89666fdaca931
Gilcare <gilcare.com@gmail.com>
2024-12-16 14:10:56 +0100

    Update CareGrid.py

--- CareGrid/CareGrid.py
+++ CareGrid/CareGrid.py
@@ -2,7 +2,7 @@ import datetime
 import streamlit as st
 #...APP INTERFACE...
 
-st.title("CareGrid")
+st.markdown("<h1 style='text-align: center;'>🪢CareGrid</h1>", unsafe_allow_html = True)
 st.caption("_wELCOME TO THE FUTURE OF HEALTHCARE_")
 
 def main():
@@ -13,6 +13,7 @@ def main():
     reg_px, view_ehr  = st.columns(2, vertical_alignment = "bottom" )
     reg_px.button("Register New Patient")
     view_ehr.button("Access Patient's Records")
+    #if reg_px.butt is 
 
   
   if app == "🗓️Schedule Appointment":
