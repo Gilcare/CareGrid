9ec98111445418a276a845de0a267c4ae98d9c33
Gilcare <gilcare.com@gmail.com>
2024-12-16 13:00:15 +0100

    Update CareGrid.py

--- CareGrid/CareGrid.py
+++ CareGrid/CareGrid.py
@@ -1,25 +1,3 @@
 import streamlit as st
 
-#...PAGE SETUP...
-home = st.Page(
-  page = "CareGrid/home.py",
-  title = "None",
-  icon = "🏠",
-  default = True)
-user = st.Page(
-  page = "CareGrid/user.py",
-  title = "None",
-  icon = "🎭",
-  )
-about = st.Page(
-  page = "CareGrid/about.py",
-  title = "None",
-  icon = "🧩",
-  )
-
-
-#...NAVIGATION SETUP...
-pg = st.navigation(pages = [home,user,about])
-
-#...RUN NAVIGATION...
-pg.run()
+app = st.selectbox
