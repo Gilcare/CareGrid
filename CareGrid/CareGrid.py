565e714197495890d7af364370f7273335189ff5
Gilcare <gilcare.com@gmail.com>
2024-12-16 13:32:45 +0100

    CareGrid.py

--- CareGrid/CareGrid.py
+++ CareGrid/CareGrid.py
@@ -1,3 +1,19 @@
+import datetime
 import streamlit as st
+#...APP INTERFACE...
+def landing_page():
+  app = st.sidebar.selectbox("Menu",("🏠Home","🗓️Schedule Appointment","🧩About"))
+  
+  if app == "🏠Home":
+    st.text_input("Search Files")
+    reg_px, view_ehr  = st.column(2, vertical_alignment = "bottom" )
+    reg_px = st.button("Register New Patient")
+    view_ehr = st.button("Access Patient's Records")
 
-app = st.selectbox
+  
+  if app == "🗓️Schedule Appointment":
+    st.caption(⚠️
+    appointment = st.date_input("")
+    
+    
+    
