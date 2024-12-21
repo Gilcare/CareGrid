e4b1f3ab18f530c09c7d6faa9ea84af5ec891b06
Gilcare <gilcare.com@gmail.com>
2024-12-21 18:05:32 +0100

    Update CareGrid.py

--- CareGrid/CareGrid.py
+++ CareGrid/CareGrid.py
@@ -54,14 +54,14 @@ def main():
   if app == "🎨EHR":
     #... PATIENT'S DASHBOARD...
     with st.expander("Patient's Details"):
-    col1, col2, col3 = st.columns(3)
-    col1.metric(label="Name", value="Adelaide Hawkins")
-    col2.metric(label="Age", value=29)
-    col3.metric(label="Sex", value="Female")
-    col4, col5 = st.columns(2)
-    col4.metric(label="Occupation", value="Realtor")
-    col5.metric(label="Ward", value="FMW")
-    col5.metric(label="🛑Drug Allergy", value="Rocephim")
+      col1, col2, col3 = st.columns(3)
+      col1.metric(label="Name", value="Adelaide Hawkins")
+      col2.metric(label="Age", value=29)
+      col3.metric(label="Sex", value="Female")
+      col4, col5 = st.columns(2)
+      col4.metric(label="Occupation", value="Realtor")
+      col5.metric(label="Ward", value="FMW")
+      col5.metric(label="🛑Drug Allergy", value="Rocephim")
 
     
     """st.markdown("#### Patient's Details")
