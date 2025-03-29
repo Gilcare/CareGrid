d58d7687e771fa5ce62da65dcd107bfa97df20c7
Gilcare <gilcare.com@gmail.com>
2025-03-29 11:15:44 +0100

    Update CareGrid.py

--- CareGrid/CareGrid.py
+++ CareGrid/CareGrid.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-if not st.experimental_user.is_logged_in():
+if not st.experimental_user.is_logged_in:
     st.login("auth0")
 else:
     st.write(f"Hello, {st.experimental_user.name}!")
