60303b08265345e04865a62a75aff2b484cd9469
Gilcare <gilcare.com@gmail.com>
2025-03-29 15:29:34 +0100

    Update CareGrid.py

--- CareGrid/CareGrid.py
+++ CareGrid/CareGrid.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 
 st.title("CareGrid")
@@ -7,8 +8,9 @@ if not st.experimental_user.is_logged_in:
     st.login("auth0")
 else:
     st.write(f"Hello, {st.experimental_user.name}!")
-
+st.divider()
 st.markdown("<p1> style = 'text-align: center;'>Continue As:</p1>", unsafe_allow_html = True)
+
 
 #app = 
 
