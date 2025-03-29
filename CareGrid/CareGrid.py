3103e99cc38e31e963f675876d8cc4554e697404
Gilcare <gilcare.com@gmail.com>
2025-03-29 15:06:20 +0100

    Update CareGrid.py

--- CareGrid/CareGrid.py
+++ CareGrid/CareGrid.py
@@ -6,8 +6,8 @@ st.write("Carly")
 if not st.experimental_user.is_logged_in:
     st.login("auth0")
 else:
+    if st.button("Log out"):
+        st.logout()
     st.write(f"Hello, {st.experimental_user.name}!")
 
     st.title("Carly")
-
-
