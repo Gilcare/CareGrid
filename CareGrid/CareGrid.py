69528224200d8fd3a4a3fb9c8055efb9fa87f673
Gilcare <gilcare.com@gmail.com>
2025-04-03 21:58:19 +0100

    Update CareGrid.py

--- CareGrid/CareGrid.py
+++ CareGrid/CareGrid.py
@@ -28,9 +28,9 @@ def healthworker_login_form():
             submitted = st.form_submit_button("Submit")
             if submitted:
                 if not healthworker_username or not healthworker_password:
-                    st.error("Invalid Username/Password")
+                    st.error("Enter Username & Password")
                 else:
-                    hw_details = credentials_collection.find_one({"Username": healthworker_username})
+                    hw_details = credentials_collection.find_one({"Username": healthworker_username, "Password": healthworker_password})
             st.write("Access Granted")
 
 
@@ -49,3 +49,53 @@ st.divider()
 #st.caption("_WELCOME TO THE FUTURE OF HEALTHCARE")
 
 
+def login_signup_page():
+    tab1, tab2, tab3 = st.tabs(["Login", "Sign-Up", "💕 Partner Console"])
+
+    with tab1:
+        with st.form(key="Login", clear_on_submit=True):
+            st.subheader("Login")
+            user = st.text_input("Username")
+            passkey = st.text_input("Password", type="password")
+
+            if st.form_submit_button("Login"):
+                if not user or not passkey:
+                    st.error("Enter both username and password")
+                else:
+                    user_details = collection.find_one({"Name": user, "Password": passkey})
+                    if not user_details:
+                        st.error("Invalid Username/Password")
+                    else:
+                        st.success("Login successful!")
+                        st.session_state.logged_in = True
+                        st.session_state.username = user
+                        st.session_state.need_to_enter_symptoms = False  # Reset state
+                        st.rerun()
+
+    with tab2:
+        with st.form(key="Sign Up", clear_on_submit=True):
+            st.subheader("Sign-Up")
+            username = st.text_input("Username")
+            email = st.text_input("Email")
+            password = st.text_input("Password", type="password")
+
+            if st.form_submit_button("Create Account"):
+                if not username or not email or not password:
+                    st.error("Please fill in all fields.")
+                elif not validate_username(username):
+                    st.error("Username can only contain letters and numbers.")
+                elif not validate_email(email):
+                    st.error("Please enter a valid email address.")
+                elif len(password) < 6:
+                    st.error("Password must be at least 6 characters long.")
+                else:
+                    if collection.find_one({"$or": [{"Name": username}, {"Email": email}]}):
+                        st.error("Username or Email already exists.")
+                    else:
+                        data = {"Name": username, "Email": email, "Password": password}
+                        added_doc = collection.insert_one(data)
+                        st.success("Account Created")
+                        st.session_state.logged_in = True
+                        st.session_state.username = username
+                        st.session_state.need_to_enter_symptoms = True  # Set state to enter symptoms
+                        st.rerun()
