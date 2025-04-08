c537a9d7de6d12a32a288f89bac90b3c92769c84
Gilcare <gilcare.com@gmail.com>
2025-04-08 11:35:57 +0100

    Update user_dashboard.py

--- CareGrid/pages/user_dashboard.py
+++ CareGrid/pages/user_dashboard.py
@@ -3,3 +3,9 @@ import streamlit as st
 
 
 
+
+# ==== INITIALIZE SESSION STATE ====
+if "user_logged_in" not in session_state:
+  st.session_state["user_logged_in"] = False
+if "user_sign_up" not in session_state:
+  st.session_state["user_sign_up"] = False
